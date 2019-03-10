22c0c123dd2b7808fcef7533d6bac6fc1fbc08e2
Fenil Suchak <fenu1998@gmail.com>
2019-03-10 12:59:04 +0530

    Docstring

--- Task_1_2_3.py
+++ Task_1_2_3.py
@@ -10,14 +10,29 @@ import os
 import sys
 
 def print_time_for_timezone(timezone, time_sec):
+    """ Function for printing time in the given timezone 
+        Parameters
+        ----------
+        timezone : str
+            Timezone for which time has to be printed.
+        time : float
+            Time in Seconds
+    """
     datetimeob_utc = datetime.utcfromtimestamp(time_sec)
     print("TIME-UTC",datetimeob_utc)
-
     timezone_cern = pytz.timezone(timezone)
     datetimeob_cern = pytz.utc.localize(datetimeob_utc, is_dst=None).astimezone(timezone_cern)
     print("TIME",timezone, datetimeob_cern)
 
 def check_function(name, node):
+    """ Function for traversing a given hdf5 file
+        Parameters
+        ----------
+        name : h5py object
+            Object returned after hdf5 file opened with h5py
+        node : h5py object
+            Either Group or Dataset of hdf5 format
+    """
     if isinstance(node, h5py.Dataset):
         try:
             temp = [str(node.name), 'Dataset', node.shape, node.size, str(node.dtype)]
@@ -30,6 +45,12 @@ def check_function(name, node):
         info_list.append(temp)
 
 def make_dataframe_from_hdf5(file_main):
+    """ Function for creating dataframe from hdf5 hierarical format
+        Parameters
+        ----------
+        file_main : h5py object
+            Object returned after hdf5 file opened with h5py
+    """
     file_main.visititems(check_function)
     info_dataframe_copy = info_dataframe.append(info_list)
     info_dataframe_copy.rename(columns = {0: 'Name',1: 'Type',2: 'Shape' ,3: 'Size', 4:'DataType'}, inplace = True)
@@ -37,6 +58,19 @@ def make_dataframe_from_hdf5(file_main):
     info_dataframe_copy.to_csv('Task2.csv', index = False)
 
 def reshape_process_and_save(imagedata_dataset, imageheight_dataset, imagewidth_dataset,file_main):
+    """ Function for reading data from path from h5py parser, reshaping
+        1-D array to 2-D array applying filter and saving as image.
+        Parameters
+        ----------
+        imagedata_dataset: String
+            Path to image data in hdf5 file
+        imageheight_dataset: String
+            Path to image height in hdf5 file
+        imagewidth_dataset: String
+            Path to image width in hdf5 file
+        file_main : h5py object
+            Object returned after hdf5 file opened with h5py
+    """
     image_1d = np.array(file_main.get(imagedata_dataset))
     height = list(file_main.get(imageheight_dataset))[0]
     width = list(file_main.get(imagewidth_dataset))[0]
@@ -46,6 +80,9 @@ def reshape_process_and_save(imagedata_dataset, imageheight_dataset, imagewidth_
     plt.imsave('Task3.png', image_final_processed)
 
 def main():
+    """
+       Main function to excecute all tasks
+    """
     global info_list, info_dataframe
     files = os.listdir()
     try:
